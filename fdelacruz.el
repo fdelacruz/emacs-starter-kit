6576ee73e11a93007b37c56b5a6591223ed2d7eb
Francisco De La Cruz <helloquico@gmail.com>
2010-07-05 18:39:36 -0400

    My initial commit

---
+++ fdelacruz.el
@@ -0,0 +1,51 @@
+;; DESCRIPTION: fdelacruz settings
+
+;; Color Theme
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme/"))
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-dark-laptop)
+
+;; Snippets
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/yasnippet"))
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+
+;; Org-mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(global-font-lock-mode 1)                     ; for all buffers
+(add-hook 'org-mode-hook 'turn-on-font-lock)  ; Org buffers only
+
+;; Stop creating those backup~ files 
+(setq make-backup-files nil) 
+
+;; Invoke M-x without the Alt key
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+;; M-x compile for cpp files
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (unless (file-exists-p "Makefile")
+              (set (make-local-variable 'compile-command)
+                   (let ((file (file-name-nondirectory buffer-file-name)))
+                     (format "%s -o %s %s %s"
+                             (or (getenv "CC") "g++")
+                             (file-name-sans-extension file)
+                             (or (getenv "CFLAGS") "-Wall -gstabs")
+                             file))))))
+;; M-x compile for java files
+(add-hook 'java-mode-hook
+          (lambda ()
+            (unless (file-exists-p "Makefile")
+              (set (make-local-variable 'compile-command)
+                   (let ((file (file-name-nondirectory buffer-file-name)))
+                     (format "%s %s"
+                             (or  "javac")
+                             file))))))
+
