9f72598e38d298787154c6c87b005c586a67a7f0
Francisco De La Cruz <helloquico@gmail.com>
2012-05-15 23:33:29 -0400

    Python syntax/error highlighting with flymake-cursor.el and pip modules epylint, pyflakes, and pep8

--- fdelacruz.el
+++ fdelacruz.el
@@ -83,3 +83,33 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/python-mode.el")
 (setq py-install-directory "~/.emacs.d/vendor/python-mode.el")
 (require 'python-mode)
+  
+;; Python sintax/error highlighting 
+(setq flymake-log-level 2) ;;Controls logging output
+
+(add-to-list 'load-path "~/.emacs.d/vendor/flymake-cursor.el")
+
+(require 'flymake-cursor)
+(global-set-key [f4] 'flymake-goto-next-error)
+
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pycheckers"  (list local-file)))) ;; shell script in ~/bin
+  
+  (add-to-list 'flymake-allowed-file-name-masks
+                            '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
+
+
+
+
+
+
+
